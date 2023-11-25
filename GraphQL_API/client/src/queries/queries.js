a4c6df817af9542dcbdfab5174e78b5aaa495e56
NikkiMerena <alderman.nikki1022@gmail.com>
2023-11-25 05:55:14 -0600

    Implement form and GraphQL mutation in AddProject component

--- GraphQL_API/client/src/queries/queries.js
+++ GraphQL_API/client/src/queries/queries.js
@@ -18,4 +18,22 @@ const getProjectsQuery = gql`
   }
 `;
 
-export { getTasksQuery, getProjectsQuery };
+const addTaskMutation = gql`
+  mutation AddTask($title: String!, $weight: Int!, $description: String!) {
+    addTask(title: $title, weight: $weight, description: $description) {
+      id
+      title
+    }
+  }
+`;
+
+const addProjectMutation = gql`
+  mutation AddProject($title: String!, $weight: Int!, $description: String!) {
+    addProject(title: $title, weight: $weight, description: $description) {
+      id
+      title
+    }
+  }
+`;
+
+export { getTasksQuery, getProjectsQuery, addTaskMutation, addProjectMutation };
