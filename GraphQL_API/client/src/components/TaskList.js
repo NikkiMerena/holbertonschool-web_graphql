670ff5dd5ef9e9928958bea849c7e6c6235a4cb9
NikkiMerena <alderman.nikki1022@gmail.com>
2023-11-25 15:11:54 -0600

    Implement task selection in TaskList component

--- GraphQL_API/client/src/components/TaskList.js
+++ GraphQL_API/client/src/components/TaskList.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
 import { getTasksQuery } from '../queries/queries';
+import TaskDetails from './TaskDetails';
 
 function TaskList(props) {
+  const [selectedTaskId, setSelectedTaskId] = useState(null);
+
   function displayTasks() {
     const data = props.data;
 
@@ -10,15 +13,22 @@ function TaskList(props) {
       return <div>Loading tasks...</div>;
     } else {
       return data.tasks.map(task => {
-        return <li key={task.id}>{task.title}</li>;
+        return (
+          <li key={task.id} onClick={() => setSelectedTaskId(task.id)}>
+            {task.title}
+          </li>
+        );
       });
     }
   }
 
   return (
-    <ul>
-      {displayTasks()}
-    </ul>
+    <div>
+      <ul>
+        {displayTasks()}
+      </ul>
+      <TaskDetails taskId={selectedTaskId} />
+    </div>
   );
 }
 
