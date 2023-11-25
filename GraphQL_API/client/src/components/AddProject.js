fa5979224ba0156f09a6cd0328b92b55a9aaee8b
NikkiMerena <alderman.nikki1022@gmail.com>
2023-11-25 05:56:05 -0600

    task12

--- GraphQL_API/client/src/components/AddProject.js
+++ GraphQL_API/client/src/components/AddProject.js
@@ -1,7 +1,7 @@
-import {
-  useState,
-  //useEffect
-} from "react";
+import { useState } from 'react';
+import { addProjectMutation, getProjectsQuery } from '../queries/queries.js';
+import { graphql } from 'react-apollo';
+import { flowRight as compose } from 'lodash';
 
 
 function AddProject(props) {
@@ -15,61 +15,63 @@ function AddProject(props) {
 
   const handleChange = (e) => {
     const newInputsProject = {
-      ...inputsProject
+      ...inputsProject,
+      [e.target.name]: e.target.name === "weight" ? parseInt(e.target.value, 10) : e.target.value
     };
-    if (e.target.name === "weight") newInputsProject[e.target.name] = parseInt(e.target.value)
-    else newInputsProject[e.target.name] = e.target.value
-    setInputsProject(newInputsProject)
+    setInputsProject(newInputsProject);
   }
 
-  return ( <
-    form class = "project"
-    id = "add-project"
-    /*onSubmit = {...}*/ >
-    <
-    div className = "field" >
-    <
-    label > Project title: < /label> <
-    input type = "text"
+  const submitForm1 = (e) => {
+    e.preventDefault();
+    props.addProjectMutation({
+      variables: {
+        title: inputsProject.title,
+        weight: inputsProject.weight,
+        description: inputsProject.description,
+      },
+      refetchQueries: [{ query: getProjectsQuery }],
+    }).then(({ data }) => {
+      console.log('Project added:', data);
+      setInputsProject({ title: '', weight: 1, description: '' });
+    }).catch((error) => {
+      console.error('Error adding project:', error);
+    });
+  };
+
+
+  return(
+  <form className = "project" id = "add-project" onSubmit = { submitForm1 } >
+    <div className = "field" >
+    <label> Project title: </label>
+    <input
+    type = "text"
     name = "title"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputsProject.title
-    }
-    / > < /
-    div > <
-    div className = "field" >
-    <
-    label > Weight: < /label> <
-    input type = "number"
+    onChange = {handleChange}
+    value = {inputsProject.title}
+    />
+    </div >
+    <div className = "field" >
+    <label > Weight: </label>
+    <input
+    type = "number"
     name = "weight"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputsProject.weight
-    }
-    / > < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > description: < /label> <
-    textarea name = "description"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputsProject.description
-    }
-    / > < /
-    div >
-    <
-    button > + < /button> < /
-    form >
+    onChange = {handleChange}
+    value = {inputsProject.weight}
+    / >
+    </div >
+    <div className = "field" >
+    <label > description: </label>
+    <textarea name = "description"
+    onChange = {handleChange}
+    value = {inputsProject.description}
+    / >
+    </div >
+    <button > + </button>
+    </form >
   );
 }
 
-export default AddProject;
+export default compose(
+  graphql(addProjectMutation, { name: "addProjectMutation" }),
+  graphql(getProjectsQuery, { name: "getProjectsQuery" })
+)(AddProject);
--- GraphQL_API/client/src/components/AddTask.js
+++ GraphQL_API/client/src/components/AddTask.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { graphql } from 'react-apollo';
-import { getProjectsQuery } from '../queries/queries';
+import { graphql } from "react-apollo";
+import React, { useState } from "react";
+import { flowRight as compose } from "lodash";
+import { getProjectsQuery, getTasksQuery, addTaskMutation } from "../queries/queries";
 
 
 function AddTask(props) {
@@ -15,12 +16,78 @@ function AddTask(props) {
       });
     }
   }
+  const [inputs, setInputs] = useState({
+    title: '',
+    weight: 1,
+    description: '',
+    projectId: ''
+  });
+
+  const handleChange = (e) => {
+    const newInputs = {
+      ...inputs
+    };
+    if (e.target.name === "weight") newInputs[e.target.name] = parseInt(e.target.value)
+    else newInputs[e.target.name] = e.target.value
+    setInputs(newInputs)
+}
+
+  const subitForm = (e) => {
+    e.preventDefault();
+    props.addTaskMutation({
+      variables: {
+        title: inputs.title,
+        weight: parseInt(inputs.weight),
+        description: inputs.description,
+        projectId: inputs.projectId
+      },
+      refetchQueries: [{ query: getTasksQuery }]
+    });
+  }
 
   return (
-    <select>
-      {displayProjects()}
-    </select>
+    <form className = "task" id = "add-task" onSubmit = {subitForm}>
+      <div className = "field" >
+      <label > Task title: </label>
+      <input type = "text"
+      name = "title"
+      onChange = {
+        handleChange
+      }
+      value = {
+        inputs.title
+      }
+      required/>
+      </div>
+      <div className = "field" >
+      <label > Weight: </label>
+      <input type = "number"
+      name = "weight"
+      onChange = {
+        handleChange
+      }
+      value = {
+        inputs.weight
+    }
+    required/>
+    </div>
+      <div className="field">
+        <label>Project:</label>
+        <select
+          name="projectId"
+          onChange={handleChange}
+          value={inputs.projectId}
+          required>
+          <option value="" disabled>Select project</option>
+          {displayProjects()}
+        </select>
+      </div>
+      <button>+</button>
+    </form>
   );
 }
 
-export default graphql(getProjectsQuery)(AddTask);
+export default compose(
+  graphql(getProjectsQuery, { name: "getProjectsQuery" }),
+  graphql(addTaskMutation, { name: "addTaskMutation" })
+)(AddTask);
